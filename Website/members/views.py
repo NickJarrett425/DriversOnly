d72420b26574eaa78f3ec5518d1c7592ccd4ac4f
David Bradley <dabradl@clemson.edu>
2023-10-01 16:03:48 -0400

    fixed an oopsie

--- Website/members/views.py
+++ Website/members/views.py
@@ -21,7 +21,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.success(request, ("You were successfully logged out."))
-    return redirect('login/')
+    return redirect('/')
 
 def register_user(request):
     if request.method == "POST":
