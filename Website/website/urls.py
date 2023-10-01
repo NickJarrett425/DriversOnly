a436901e45e53d1aa8817c0886e2dc3ccbdee772
David Bradley <dabradl@clemson.edu>
2023-10-01 15:27:52 -0400

    changes to navbar and urls

--- Website/website/urls.py
+++ Website/website/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('members/', include('django.contrib.auth.urls')),
     path('', include('members.urls')),
 ]
