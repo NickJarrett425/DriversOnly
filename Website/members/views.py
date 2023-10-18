c229fe848ca10c12ef9cc0222a8f53141be5159b
David Bradley <dabradl@clemson.edu>
2023-10-18 16:52:53 -0400

    Fixed error with unauthenticated user accessing a profile

--- Website/members/views.py
+++ Website/members/views.py
@@ -48,6 +48,9 @@ def register_user(request):
     return render(request, 'registration/register_user.html', {'form':form,})
 
 def view_profile(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "You need to log in or register to view your profile")
+        return redirect('/')
     profile, created = UserProfile.objects.get_or_create(user=request.user)
     if request.user.is_superuser:
         return render(request, 'registration/admin_profile.html', {'profile': profile})
@@ -75,6 +78,9 @@ def edit_profile(request):
         return render(request, 'registration/edit_profile.html', {'profile': profile, 'form': form})
 
 def view_driver_profile(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "You need to log in or register to view your profile")
+        return redirect('/')
     try:
         driver, created = DriverProfile.objects.get_or_create(user=request.user)
     except DriverProfile.DoesNotExist:
