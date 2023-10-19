97b24bc87b029aee31dd54943939e097c5fb69d6
David Bradley <dabradl@clemson.edu>
2023-10-19 15:04:58 -0400

    Consolidating user profiles

--- Website/members/admin.py
+++ Website/members/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import DriverProfile
+from .models import DriverProfile, UserProfile
 
+admin.site.register(UserProfile)
 admin.site.register(DriverProfile)
--- Website/members/forms.py
+++ Website/members/forms.py
@@ -28,4 +28,27 @@ class UserProfileForm(forms.ModelForm):
 class DriverProfileForm(UserProfileForm):
     class Meta:
         model = DriverProfile
-        fields = ['first_name', 'last_name', 'middle_initial', 'email', 'street_address', 'city', 'state', 'zipcode', 'phone_number', 'email', 'date_of_birth', 'drivers_license', 'year', 'make', 'model', 'vin', 'provider_name', 'policy_number', 'emergency_contact_name', 'emergency_contact_phone']
+        fields = ['first_name', 'middle_initial', 'last_name', 'phone_number', 'email', 'date_of_birth', 'street_address', 'city', 'state', 'zipcode', 'drivers_license', 'year', 'make', 'model', 'vin', 'provider_name', 'policy_number', 'emergency_contact_name', 'emergency_contact_phone']
+    def __init__(self, *args, **kwargs):
+        super(DriverProfileForm, self).__init__(*args, **kwargs)
+        
+        self.fields['first_name'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['middle_initial'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['phone_number'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['email'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['date_of_birth'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['street_address'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['city'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['state'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['zipcode'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['drivers_license'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['year'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['make'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['model'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['vin'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['provider_name'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['policy_number'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['emergency_contact_name'].widget.attrs['class'] = 'form-control-sm'
+        self.fields['emergency_contact_phone'].widget.attrs['class'] = 'form-control-sm'
+
