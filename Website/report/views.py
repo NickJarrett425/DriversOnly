ca689786e0715d7a776a986436c241810d0082f6
Neo0152 <113534226+Neo01521@users.noreply.github.com>
2023-10-25 11:08:23 -0400

    change #2

--- Website/report/views.py
+++ Website/report/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import about
 
-# Create your views here.
+def about_page(request):
+    newestAbout = about.objects.latest('release_date')
+    return render(request, 'about/about_page.html', 
+                  {'newestAbout' : newestAbout})
