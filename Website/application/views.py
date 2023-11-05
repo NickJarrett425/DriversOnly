e953cf87c0c7cdde4a45cf5e15759345077d750b
David Bradley <dabradl@clemson.edu>
2023-11-05 13:36:23 -0500

    Fixed no change when editing an application

--- Website/application/views.py
+++ Website/application/views.py
@@ -174,9 +174,9 @@ def application_edit(request, id):
         user=application.driver.user
         driver = DriverProfile.objects.get(user=user)
         if request.method == 'POST':
-            form = ApplicationForm(request.POST, instance=application)
+            form = ApplicationForm(request.POST, instance=driver)
             if form.is_valid():
-                application = form.save()
+                application = form.save(commit=False)
                 application.save()
                 return redirect('/application/review/'+str(id))
         else:
