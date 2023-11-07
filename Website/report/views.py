ea5b03b3f4cc171cfe90831dde67debd842ff513
Neo0152 <113534226+Neo01521@users.noreply.github.com>
2023-11-07 10:47:15 -0500

    Trying to make the pdf look alot nicer for users

--- Website/report/views.py
+++ Website/report/views.py
@@ -6,9 +6,21 @@ import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate,Table,TableStyle,Image 
+from reportlab.lib import colors
+import os
+from django.conf import settings
+
 
 
 # Create your views here.
+def build_pdf_page(canvas, doc):
+    rel_path = "static/logo.png"
+    file_path = os.path.join(settings.STATICFILES_DIRS, rel_path)
+    logo = Image(str(file_path), width=200, height=200)
+    canvas.drawImage(logo)
+
+
 
 def all_login_attempts(request):
     login_list = login_log.objects.all()
@@ -43,26 +55,47 @@ def all_login_attempts_download_csv(request):
 
 def all_login_attempts_download_pdf(request):
     buff = io.BytesIO()
-    can = canvas.Canvas(buff, pagesize=letter, bottomup=0)
-    textobj = can.beginText()
-    textobj.setTextOrigin(inch,inch)
-    textobj.setFont("Times-Roman", 12)
+    doc = SimpleDocTemplate(buff, pagesize=letter)
+    doc.build([], onFirstPage=build_pdf_page, onLaterPages=build_pdf_page)
 
-    lines = []
+    # Headers
     login_list = login_log.objects.all()
 
+    table_header = ['Date and Time', 'Username', 'Login Success']
+
+    data = [table_header]
+
     for log in login_list:
-        lines.append(str(log.Datestamp))
-        lines.append(log.username)
-        lines.append(str(log.login_success))
-        lines.append(" ")
+        formatted_date = log.Datestamp.strftime('%m-%d-%Y %H:%M')
+        login_result = bool(log.login_success)
+        row = [formatted_date, log.username, login_result]
+        data.append(row)
 
-    for line in lines: 
-        textobj.textLine(line)
+    common_row_height = 25
+    page_height = letter[1]
+    max_rows_per_page = int((page_height - 100) / common_row_height)
 
-    can.drawText(textobj)
-    can.showPage()
-    can.save()
+    table = Table(data, repeatRows=1)
+    table.setStyle(TableStyle([
+        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#B3B6B7")),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+        ('BACKGROUND', (1, 1), (1, -1), colors.HexColor("#E6E6E6")),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+    ]))
+
+    elements = []
+    elements.append(table)
+
+    doc.build(elements)
     buff.seek(0)
+
     
-    return FileResponse(buff, as_attachment=True, filename='logins.pdf')
+    doc.showPage()
+    
+
+
+
+    return HttpResponse(buff, content_type='application/pdf')
