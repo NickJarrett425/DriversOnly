ae078988bcc43b671b0c1ad8470ac906aa8aa1b7
Neo0152 <113534226+Neo01521@users.noreply.github.com>
2023-11-07 14:43:13 -0500

    added the password change log model and chagned login_log model

--- Website/report/models.py
+++ Website/report/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class login_log(models.Model):
+class log(models.Model):
     Datestamp = models.DateTimeField(auto_now=True)
     username = models.CharField(max_length=150,null=False,blank=False,default="test")
+
+class login_log(log):
     login_success = models.BooleanField(null=False)
+
+class password_change_log(log):
+    password_change_type = models.BooleanField(null=False)
