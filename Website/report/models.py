170aa467cbb698e62d445c33e9c8662719afd099
Neo0152 <113534226+Neo01521@users.noreply.github.com>
2023-11-07 14:53:42 -0500

    reverted login_log model and migrated passworl_change_log

--- Website/report/models.py
+++ Website/report/models.py
@@ -6,7 +6,9 @@ class log(models.Model):
     Datestamp = models.DateTimeField(auto_now=True)
     username = models.CharField(max_length=150,null=False,blank=False,default="test")
 
-class login_log(log):
+class login_log(models.Model):
+    Datestamp = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=150,null=False,blank=False,default="test")
     login_success = models.BooleanField(null=False)
 
 class password_change_log(log):
