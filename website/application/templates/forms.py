46d971a16f3dbd87c9e463ea040b3423aab0b1a8
Ryan Ivie <rivie@clemson.edu>
2023-12-04 02:31:55 +0000

    Added 8 files to /website/application/templates

---
+++ website/application/templates/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Application
+from Catalog.models import Product
+#from members.models import SponsorList
+from members.models import SponsorOrganization
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = ['sponsor_name', 'first_name', 'last_name', 'middle_initial', 'email', 'phone', 'street_address', 'city', 'state', 'zipcode', 'license_num', 'plate_num', 'year', 'make', 'model', 'vin', 'provider_name', 'policy_number']
+
+    sponsor_name = forms.ChoiceField()
+
+    def __init__(self, *args, **kwargs):
+        super(ApplicationForm, self).__init__(*args, **kwargs)
+        self.fields['sponsor_name'].choices = self.get_choices()
+
+    def get_choices(self):
+        #choices = SponsorList.objects.values_list('sponsor_name', 'sponsor_name').distinct()
+        choices = SponsorOrganization.objects.values_list('organizationName', 'organizationName').distinct()
+        return choices
+
+class ApplicatonReasonForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = ['application_reason']
+
+    def __init__(self, *args, **kwargs):
+        super(ApplicatonReasonForm, self).__init__(*args, **kwargs)
+
+        self.fields['application_reason'].widget.attrs['class'] = 'form-control'
