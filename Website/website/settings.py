9f7ac1d09c35c59c31e1635e7d440ba29382ffce
Nicholas Jarrett <ndjarre@clemson.edu>
2024-01-15 12:08:22 -0500

    Renewed email functionality

--- Website/website/settings.py
+++ Website/website/settings.py
@@ -130,8 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')  # Directory for collec
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Server Info
-#EMAIL_BACKEND = 'django_ses.SESBackend'
-#AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-#AWS_SES_REGION_NAME = 'us-east-1'
-#AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
+EMAIL_BACKEND = 'django_ses.SESBackend'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_SES_REGION_NAME = 'us-east-1'
+AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
